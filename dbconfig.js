8950ee8c21a59e4403b8bff4ac7004c2c71ab5e1
Hariprasathh <hariimm997@gmail.com>
2023-10-30 20:52:35 +0530

    new

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,24 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/GoldRateConverter').then(()=>{
-    console.log('Connected to the database');
-}).catch((err)=>{
-    console.log(err);
-})
+// mongoose.connect('mongodb://127.0.0.1:27017/GoldRateConverter').then(()=>{
+//     console.log('Connected to the database');
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+
+const mongoose = require("mongoose");
+
+
+const connection = async()=>{
+  await mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Connection Error: ", err);
+  });
+}
+
+module.exports =connection;
