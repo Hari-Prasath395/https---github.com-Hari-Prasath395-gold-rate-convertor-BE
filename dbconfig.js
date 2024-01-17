e892d989ee2b461f9682be4b373d8f439829fbe0
Hari-Prasath395 <hariiprasathh395@gmail.com>
2024-01-17 13:58:26 +0530

    5445

--- dbconfig.js
+++ dbconfig.js
@@ -1,11 +1,3 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb://127.0.0.1:27017/GoldRateConverter').then(()=>{
-//     console.log('Connected to the database');
-// }).catch((err)=>{
-//     console.log(err);
-// })
-
 
 const mongoose = require("mongoose");
 
