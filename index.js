26375d8ebbb0271d15cd87730de56c9786e60983
Hari-Prasath395 <hariiprasathh395@gmail.com>
2024-01-17 11:08:14 +0530

    44

--- index.js
+++ index.js
@@ -1,52 +1,60 @@
-const express = require('express');
-const nodeServer = express();
-const appServer = require('./app');
-const bodyParser =require('body-parser');
-const cors= require('cors');
-const connectDB = require('./dbconfig');
-connectDB();
-
-nodeServer.use(bodyParser.json());
-nodeServer.use(bodyParser.urlencoded({ extended: true }));
-nodeServer.use(cors());
-
-nodeServer.use('/',appServer);
-
-
-const port = process.env.PORT || 8000;
-nodeServer.listen(port,()=>{
-    console.log(`Server started on port ${port}`);
-})
-
-
 // const express = require('express');
 // const nodeServer = express();
 // const appServer = require('./app');
-// const bodyParser = require('body-parser');
-// require('./dbconfig');
-// const cors = require('cors'); // Import the cors middleware
-
-// // Set up CORS configuration to allow requests from your frontend
-// const allowedOrigins = ['https://frabjous-genie-8ba260.netlify.app']; // Add your frontend URL here
+// const bodyParser =require('body-parser');
+// const cors= require('cors');
+// const connectDB = require('./dbconfig');
+// connectDB();
 
 // nodeServer.use(bodyParser.json());
 // nodeServer.use(bodyParser.urlencoded({ extended: true }));
+// nodeServer.use(cors());
 
-// nodeServer.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     },
-//   })
-// );
+// const allowedOrigins = ['https://frabjous-genie-8ba260.netlify.app'];
 
-// nodeServer.use('/', appServer);
+// nodeServer.use('/',appServer);
+
 
 // const port = process.env.PORT || 8000;
-// nodeServer.listen(port, () => {
-//   console.log(`Server started on port ${port}`);
-// });
+// nodeServer.listen(port,()=>{
+//     console.log(`Server started on port ${port}`);
+// })
+
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./dbconfig');
+const appServer = require('./app');
+
+const nodeServer = express();
+const port = process.env.PORT || 8000;
+
+// Connect to the database
+connectDB();
+
+// Middleware
+nodeServer.use(bodyParser.json());
+nodeServer.use(bodyParser.urlencoded({ extended: true }));
+
+// CORS configuration
+const allowedOrigins = ['https://frabjous-genie-8ba260.netlify.app'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+nodeServer.use(cors(corsOptions));
+
+// Routes
+nodeServer.use('/', appServer);
+
+// Start the server
+nodeServer.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
